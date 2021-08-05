872db8cc1f594b6b9339167d30d927dcc10c820c
kelwin.rodrigues <kelwin.rodrigues@192.168.56.1>
2021-08-04 22:15:07 -0300

    Versão 1.1

--- src/programacao/Arredondamento.java
+++ src/programacao/Arredondamento.java
@@ -13,7 +13,9 @@ public class Arredondamento {
     public static void main(String[] args){
          double valor = 9.53623454235243;
          
-         System.out.println("Inteiro mais próximo ");
+        System.out.println("Inteiro mais próximo " + Math.round(valor));
+        System.out.println("Inteiro do número = " + Math.floor(valor));
+        System.out.println("Valor arredondado = " + (double) Math.round(valor * 100) / 100);
     }
     
     
